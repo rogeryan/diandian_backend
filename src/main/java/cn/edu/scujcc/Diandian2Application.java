3d47e63bf43ff47d7c8f1564a5e6b11cd36ac6d7
Roger <2749234+rogeryan@users.noreply.github.com>
2020-05-20 17:45:10 +0800

    修改注册拦截器的方法，避免使用淘汰方法。

--- src/main/java/cn/edu/scujcc/Diandian2Application.java
+++ src/main/java/cn/edu/scujcc/Diandian2Application.java
@@ -1,5 +1,6 @@
 package cn.edu.scujcc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
@@ -8,15 +9,24 @@ import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableCaching
-public class Diandian2Application {
-
+public class Diandian2Application implements WebMvcConfigurer{
+	@Autowired
+	AuthInterceptor authInterceptor;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Diandian2Application.class, args);
 	}
 	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authInterceptor);
+	}
+
 	@Bean
 	public EhCacheManagerFactoryBean ehCacheCacheManager() {
 		EhCacheManagerFactoryBean cmfb = new EhCacheManagerFactoryBean();
